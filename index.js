0d0642da8c2848ed4a3954fd92b22e6b417922e3
Sahil chaudhari <sahilchaudhari2405@gmail.com>
2022-10-31 19:37:11 +0530

    try to insert data

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const client = new Client({
     database: "signup"
 })
 client.connect();
-client.query(`INSERT INTO studentdata(studentid,firstname,lastname,email,password) value()`, (err,result) => {
-     if(!err)
-     {
-        console.log(result.rows);
-     }
-     else{
-        console.log(err)
-     }
-})
+// client.query(`INSERT INTO studentdata(studentid,firstname,lastname,email,password) value()`, (err,result) => {
+//      if(!err)
+//      {
+//         console.log(result.rows);
+//      }
+//      else{
+//         console.log(err)
+//      }
+// })
 const PORT = 3000;
 const  bodyparser = require("body-parser");
 app.use(bodyparser.urlencoded({extended:true}));
@@ -25,21 +25,14 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname+"/public/index.html");
 }); 
-app.post('signup',(req,res) => {
+app.post('/signup',(req,res) => {
     var firstnm = req.body.firstnm;
     var lastnm = req.body.lastnm;
     var email = req.body.email;
     var password = req.body.pass; 
-//     client.query(`INSERT INTO studentdata(studentid,firstname,lastname,email,password) value()`, (err,result) => {
-//         if(!err)
-//         {
-//            console.log(result.rows);
-//         }
-//         else{
-//            console.log(err)
-//         }
-//    })
-console.log(firstnm);
+    client.query("INSERT INTO studentdata(firstname, lastname, email, password) value($1,$2,$3,$4)", [firstnm,lastnm,email,password]).then(data => {
+        res.send(data);});
+        res.send(firstnm);
 })
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
